9789729bda99130ba8ed3778075990b949876c65
horsicq <horsicq@gmail.com>
2022-02-03 18:27:46 +0100

    Fix: 2022-02-03

--- global.h
+++ global.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017-2021 hors<horsicq@gmail.com>
+/* Copyright (c) 2017-2022 hors<horsicq@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,7 +25,7 @@
 
 #define X_APPLICATIONDISPLAYNAME "XNTSV"
 #define X_APPLICATIONNAME "xntsv"
-#define X_APPLICATIONVERSION "3.01"
+#define X_APPLICATIONVERSION "3.02"
 #define X_ORGANIZATIONNAME "NTInfo"
 #define X_ORGANIZATIONDOMAIN "ntinfo.biz"
 #define X_OPTIONSFILE "xntsv.ini"
