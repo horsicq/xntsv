594e098efa28fa2c54bba9083f908893879b64dc
horsicq <horsicq@gmail.com>
2022-05-19 20:19:56 +0200

    Fix: 2022-05-19

--- gui_source/guimainwindow.cpp
+++ gui_source/guimainwindow.cpp
@@ -58,11 +58,11 @@ GuiMainWindow::GuiMainWindow(QWidget *pParent) :
 
     g_xShortcuts.setName(X_SHORTCUTSFILE);
 
-    g_xShortcuts.addGroup(XShortcuts::ID_PROCESS);
-    g_xShortcuts.addGroup(XShortcuts::ID_DISASM);
-    g_xShortcuts.addGroup(XShortcuts::ID_HEX);
-    g_xShortcuts.addGroup(XShortcuts::ID_STRINGS);
-    g_xShortcuts.addGroup(XShortcuts::ID_SIGNATURES);
+    g_xShortcuts.addGroup(XShortcuts::GROUPID_PROCESS);
+    g_xShortcuts.addGroup(XShortcuts::GROUPID_DISASM);
+    g_xShortcuts.addGroup(XShortcuts::GROUPID_HEX);
+    g_xShortcuts.addGroup(XShortcuts::GROUPID_STRINGS);
+    g_xShortcuts.addGroup(XShortcuts::GROUPID_SIGNATURES);
 
     g_xShortcuts.load();
 
